7a651615e5279a5def4ae68978bf3225c412eb98
AustinNielsen <austinnielsen@unomaha.edu>
2019-09-10 23:39:05 -0500

    img test

--- src/App.js
+++ src/App.js
@@ -161,7 +161,7 @@ class App extends Component {
               <button id="red" onClick={() => this.setColor('red')}>Red</button>
 
               <div>
-                <img class="cardImg" src="https://deckofcardsapi.com/static/img/AS.png" alt="Ace of Spades"></img>
+                <img class="cardImg" src={this.state.cards.piles.super.cards[0].image} alt="Ace of Spades"></img>
               </div>
               </div>
             : <div>
