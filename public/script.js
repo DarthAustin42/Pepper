521715e5dd9b13c6bd6f79733fdb9600a7245db0
AustinNielsen <austinnielsen@unomaha.edu>
2019-09-12 21:52:32 -0500

    Added height changes to cards

--- public/script.js
+++ public/script.js
@@ -1,6 +1,14 @@
 function onResizeFunction() {
   console.log("HEYO THERE");
   console.log($(document).width());
+  console.log($(document).height());
   var newWidth = $(document).width() * 0.07;
-  $(".cardImg").css( "width", newWidth);
+  var newHeight = $(document).height() * 0.0972461
+
+  if (newWidth < newHeight) {
+    $(".cardImg").css( "width", newWidth);
+  }
+  else {
+    $(".cardImg").css( "height", newHeight);
+  }
 }
