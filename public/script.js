c5a728d7d250d8fe83b91bb58c88220f656ed315
AustinNielsen <austinnielsen@unomaha.edu>
2019-09-12 21:57:22 -0500

    added missing ;

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ function onResizeFunction() {
   console.log($(document).width());
   console.log($(document).height());
   var newWidth = $(document).width() * 0.07;
-  var newHeight = $(document).height() * 0.0972461
+  var newHeight = $(document).height() * 0.0972461;
 
   if (newWidth < newHeight) {
     $(".cardImg").css( "width", newWidth);
