d2b066c65e6f1f93e094892ec67040a4ea51efac
AustinNielsen <austinnielsen@unomaha.edu>
2019-09-12 22:36:29 -0500

    Fixed cards position issue

--- public/script.js
+++ public/script.js
@@ -1,17 +1,9 @@
 function onResizeFunction() {
   var docWidth = $(document).width();
   var docHeight = $(document).height();
-  var deck13Width = $("#deck3").width();
-
-  console.log(docWidth);
-  console.log(deck13Width);
 
   var newWidth = docWidth * 0.06;
   var newHeight = docHeight * 0.1605643;
-
-  var deck3LeftLoc = ((docWidth - deck13Width) / 2);
-
-  console.log(deck3LeftLoc);
 
   if (docWidth < docHeight) {
     $(".cardImg").css( "width", newWidth);
@@ -23,6 +15,9 @@ function onResizeFunction() {
     newWidth = newHeight * 0.7197452;
     $(".cardImg").css( "width", newWidth);
   }
+  var deck13Width = newWidth * 6;
+
+  var deck3LeftLoc = ((docWidth - deck13Width) / 2);
 
   $("#deck3").css( "left", deck3LeftLoc );
 }
