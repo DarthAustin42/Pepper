6f792bbbe21fde0f57ac149a0d35daab65235199
AustinNielsen <austinnielsen@unomaha.edu>
2019-09-12 22:10:59 -0500

    changes

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ function onResizeFunction() {
   var docWidth = $(document).width();
   var docHeight = $(document).height();
   var newWidth = docWidth * 0.07;
-  var newHeight = docHeight * 0.17;
+  var newHeight = docHeight * 0.0972461;
 
   if (docWidth < docHeight) {
     $(".cardImg").css( "width", newWidth);
