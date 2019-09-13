dfac181bf92fb0ec308cbee4bcfb634e6f3476c3
AustinNielsen <austinnielsen@unomaha.edu>
2019-09-12 21:49:13 -0500

    Changed card size from .7 to .07

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 function onResizeFunction() {
   console.log("HEYO THERE");
   console.log($(document).width());
-  var newWidth = $(document).width() * 0.7;
+  var newWidth = $(document).width() * 0.07;
   $(".cardImg").css( "width", newWidth);
 }
