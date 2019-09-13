95f5c6792051da90f40d1d70f9994a5ca3d4407d
AustinNielsen <austinnielsen@unomaha.edu>
2019-09-12 21:48:36 -0500

    Added card size changing

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
 function onResizeFunction() {
   console.log("HEYO THERE");
+  console.log($(document).width());
+  var newWidth = $(document).width() * 0.7;
+  $(".cardImg").css( "width", newWidth);
 }
