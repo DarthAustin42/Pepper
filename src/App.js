dea7bbe0f7b96f860ccee5c99065526f6aa92f1b
AustinNielsen <austinnielsen@unomaha.edu>
2019-09-18 14:57:16 -0500

    dd

--- src/App.js
+++ src/App.js
@@ -7,23 +7,6 @@ import SignupForm from './components/SignupForm';
 import './App.css';
 import ReactDOM from 'react-dom';
 var dragula = require('react-dragula');
-var App = React.createClass({
-  render: function () {
-    return <div className='container'>
-      <div>Swap me around</div>
-      <div>Swap her around</div>
-      <div>Swap him around</div>
-      <div>Swap them around</div>
-      <div>Swap us around</div>
-      <div>Swap things around</div>
-      <div>Swap everything around</div>
-    </div>;
-  },
-  componentDidMount: function () {
-    var container = React.findDOMNode(this);
-    dragula([container]);
-  }
-});
 
 class App extends Component {
   constructor() {
